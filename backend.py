5b7086940506cc8326f08a17fcd9fe79c76ca7a1
Steve Jones <steve@kupjones.com>
2025-01-08 22:02:01 -0600

    first commit

---
+++ backend.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route('/get_info', methods=['GET'])
+def get_info():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return jsonify({"hostname": hostname, "ip_address": ip_address})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
